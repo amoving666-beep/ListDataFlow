456dabc9d897be2b3144f9012923ab7471214178
amoving666-beep <amoving666@gmail.com>
2026-04-27 22:43:42 +0800

    Commit message (required)

---
+++ DeviceManagerDemo/Modules/ProductList/Helpers/CacheHelper.swift
@@ -0,0 +1,8 @@
+//
+//  CacheHelper.swift
+//  DeviceManagerDemo
+//
+//  Created by 天亮了 on 2026/4/27.
+//
+
+import Foundation
---
+++ DeviceManagerDemo/Modules/ProductList/View/ProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductCell.swift
+//  DeviceManagerDemo
+//
+//  Created by 天亮了 on 2026/4/27.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
